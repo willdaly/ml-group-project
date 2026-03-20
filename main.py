e2c0e10ac015516148e49743b68019705b9036e6
Will <rwilliamdaly@gmail.com>
2026-03-20 03:10:01 -0400

    Add main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+"""ML Group Project
+
+ML Group project
+ML Group Project provides a practical avenue for applying theoretical knowledge, allowing us to actively engage in the model development process. Through these exercises, we will hone
+"""
+
+def deck():
+    """TODO: implement deck."""
+    raise NotImplementedError
+
+def Irvine():
+    """TODO: implement Irvine."""
+    raise NotImplementedError
+
+def Dataset():
+    """TODO: implement Dataset."""
+    raise NotImplementedError
+
+def question():
+    """TODO: implement question."""
+    raise NotImplementedError
+
+def main():
+    """Main function - implement your solution here."""
+    pass
+
+if __name__ == "__main__":
+    main()
